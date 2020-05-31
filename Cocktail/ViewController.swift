e8fd47f38dcd29f09cffedf2323701103c8f578a
mkaraa <metehan.kara95@gmail.com>
2020-05-31 17:20:54 +0300

    Pods File Configuration is done

---
+++ Cocktail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocktail
+//
+//  Created by Metehan kara on 31.05.2020.
+//  Copyright © 2020 Metehan kara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
