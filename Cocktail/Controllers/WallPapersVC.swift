6b524a4af7f9c1ce0811b783fbc0b39e927ececb
mkaraa <metehan.kara95@gmail.com>
2020-05-31 17:38:24 +0300

    Controllers are added

---
+++ Cocktail/Controllers/WallPapersVC.swift
@@ -0,0 +1,30 @@
+//
+//  WallPapersVC.swift
+//  Cocktail
+//
+//  Created by Metehan kara on 31.05.2020.
+//  Copyright © 2020 Metehan kara. All rights reserved.
+//
+
+import UIKit
+
+class WallPapersVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
