a223c7c38966b67b5358f27e574ac43f23b2d9d7
mkaraa <metehan.kara95@gmail.com>
2020-06-08 00:59:38 +0300

    DetailVC textview

--- Cocktail/Controllers/DetailVC.swift
+++ Cocktail/Controllers/DetailVC.swift
@@ -12,7 +12,7 @@ import SwiftyJSON
 import Kingfisher
 
 class DetailVC: UIViewController {
-
+    
     // MARK: IBOutlet
     
     @IBOutlet weak var imageView: UIImageView!
@@ -20,16 +20,20 @@ class DetailVC: UIViewController {
     @IBOutlet weak var textView: UITextView!
     
     var detailTitle: String?
-    var ingredients: [String] = []
     var id: String?
     var apiURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
     var selectedUrl: String?
+    
+    var ingredients: [String] = []
+    var ingredient: String?
+    var instruction: String?
+    var index = 0
     
     // MARK: viewDidLoad()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         selectedUrl = apiURL + id!
         
         
@@ -43,15 +47,15 @@ class DetailVC: UIViewController {
     
     func getDetailCoctail(){
         AF.request(selectedUrl!, method: .get).responseJSON { myresponse in
-
+            
             // check result is success or failure
             switch myresponse.result {
             case .success:
-
+                
                 // GET data
                 let myresult = try? JSON(data: myresponse.data!)
                 let resultArray = myresult!
-
+                
                 //
                 var index: Int? = 0
                 
@@ -64,26 +68,52 @@ class DetailVC: UIViewController {
                         let name = item["strDrink"].string
                         let image = item["strDrinkThumb"].string
                         let alcoholic = item["strAlcoholic"].string
-                        var instructions = item["strInstructions"].string
-                        var ingredient = item["strIngredient1"].string
-                       
+                        let instructions = item["strInstructions"].string
+                        //                        let ingredient1 = item["strIngredient1"].string
+                        //                        let ingredient2 = item["strIngredient2"].string
+                        //                        let ingredient3 = item["strIngredient3"].string
+                        //                        let ingredient4 = item["strIngredient4"].string
                         
-                        if self.segmentedControl.selectedSegmentIndex == 0 {
-                            self.textView.text = ingredient
-                        } else {
-                            self.textView.text = instructions
+                        
+                        if let isIngredientExist = item["strIngredient\(String(describing: index))"].string {
+                            self.ingredients.append(isIngredientExist)
+                            index! += 1
                         }
                         
-                        print(self.ingredients)
-    
+                        if let isIngredientExist = item["strMeasure\(String(describing: index))"].string {
+                            self.ingredients.append(isIngredientExist)
+                            index! += 1
+                        }
+                        //
+                        //                        self.ingredients.append(ingredient1!)
+                        //                        self.ingredients.append(ingredient2!)
+                        //                        self.ingredients.append(ingredient3!)
+                        //                        self.ingredients.append(ingredient4!)
+                        
+                        print("Ingredients" , self.ingredients)
+                        
+                        self.instruction = instructions
+                        
+                        let imageUrl = URL(string: image!)
+                        self.imageView.kf.setImage(with: imageUrl)
                     }
-
+                    
                     i = i + 1
                 }
-
+                
             case .failure:
                 print("Error")
             }
+        }
+    }
+    
+    @IBAction func SegmentControlAction(_ sender: Any) {
+        if self.segmentedControl.selectedSegmentIndex == 0 {
+            for cocktail in self.ingredients {
+                self.textView.text = cocktail
+            }
+        } else {
+            self.textView.text = instruction
         }
     }
     
